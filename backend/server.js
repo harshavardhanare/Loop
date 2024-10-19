639c3cc418ed4f3425dfbde4fb371c80fb31bcc2
praveen8240 <156305741+praveen8240@users.noreply.github.com>
2024-10-19 21:01:35 +0530

    bugs

--- backend/server.js
+++ backend/server.js
@@ -18,14 +18,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV !== "production") {
 	app.use(
 		cors({
-			origin: "https://loop-frontend-steel.vercel.app/",
+			origin: "https://loop-frontend-steel.vercel.app",
 			credentials: true,
 		})
 	);
-}
 
 app.use(express.json({ limit: "5mb" })); // parse JSON request bodies
 app.use(cookieParser());
