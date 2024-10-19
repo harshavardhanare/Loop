2e9725c6dca30bf1e1086da1f55fb785f8768bfb
praveen8240 <156305741+praveen8240@users.noreply.github.com>
2024-10-19 21:47:22 +0530

    updates

--- backend/server.js
+++ backend/server.js
@@ -18,20 +18,17 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use(
-    cors({
-        origin: "*", // Allows all origins, use cautiously
-        credentials: true, // Allows credentials
-    })
-);
-
+if (process.env.NODE_ENV !== "production") {
+	app.use(
+		cors({
+			origin: "http://localhost:5173",
+			credentials: true,
+		})
+	);
+}
 
 app.use(express.json({ limit: "5mb" })); // parse JSON request bodies
 app.use(cookieParser());
-
-app.get('/',(req, res) => {
-	res.send("hii");
-})
 
 app.use("/api/v1/auth", authRoutes);
 import adminRoutes from "./routes/admin.route.js";
@@ -42,6 +39,13 @@ app.use("/api/v1/notifications", notificationRoutes);
 app.use("/api/v1/connections", connectionRoutes);
 app.use("/api/v1/chat", chatRoutes);
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-	baseURL: import.meta.env.MODE === "development" ? "https://loop-backend-plum.vercel.app/api/v1" : "https://loop-backend-plum.vercel.app/api/v1",
+	baseURL:  "http://localhost:5000/api/v1" ,
 	withCredentials: true,
 });
